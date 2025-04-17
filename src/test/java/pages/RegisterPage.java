d0234caaa12cb756be6bf7d87b95cd66b447def7
crimson-king-5 <karimal-homsi@hotmail.com>
2025-04-17 10:27:01 +0200

    Modified Register Page/Steps

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -2,103 +2,111 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.*;
 public class RegisterPage extends BasePage {
-    @FindBy(tagName = "h1")
-    private WebElement formTitle;
+    @FindBy(id = "email_create")
+    private WebElement emailInput;
 
-    @FindBy(id = "customer.firstName")
+    @FindBy(id = "SubmitCreate")
+    private WebElement createAccountBtn;
+
+    @FindBy(xpath = "//div[@id='create_account_error']//li")
+    private WebElement emailErrorMessage;
+
+    // Formulaire "Create an account"
+    @FindBy(id = "id_gender1")
+    private WebElement genderMr;
+
+    @FindBy(id = "id_gender2")
+    private WebElement genderMrs;
+
+    @FindBy(id = "customer_firstname")
     private WebElement firstNameInput;
 
-    @FindBy(id = "customer.lastName")
+    @FindBy(id = "customer_lastname")
     private WebElement lastNameInput;
 
-    @FindBy(id = "customer.address.street")
-    private WebElement addressInput;
-
-    @FindBy(id = "customer.address.city")
-    private WebElement cityInput;
-
-    @FindBy(id = "customer.address.state")
-    private WebElement stateInput;
-
-    @FindBy(id = "customer.address.zipCode")
-    private WebElement zipCodeInput;
-
-    @FindBy(id = "customer.phoneNumber")
-    private WebElement phoneInput;
-
-    @FindBy(id = "customer.ssn")
-    private WebElement ssnInput;
-
-    @FindBy(id = "customer.username")
-    private WebElement usernameInput;
-
-    @FindBy(id = "customer.password")
+    @FindBy(id = "passwd")
     private WebElement passwordInput;
 
-    @FindBy(id = "repeatedPassword")
-    private WebElement repeatedPasswordInput;
+    @FindBy(id = "days")
+    private WebElement daysSelect;
 
-    @FindBy(xpath = "//p[contains(text(),'Your account was created successfully. You are now logged in.')]")
-    private WebElement succesMessage;
+    @FindBy(id = "months")
+    private WebElement monthsSelect;
 
-    @FindBy(xpath = "(//input[ @type = \"submit\"])[2]")
-    private WebElement registerButton;
+    @FindBy(id = "years")
+    private WebElement yearsSelect;
 
-    public String imRedirectedToTheSignupPage() {
-        return formTitle.getText();
+    @FindBy(id = "newsletter")
+    private WebElement newsletterCheckbox;
+
+    @FindBy(id = "submitAccount")
+    private WebElement registerBtn;
+
+    @FindBy(css = ".account")
+    private WebElement accountHeader;
+
+    @FindBy(css = ".info-account")
+    private WebElement successMessage;
+
+    @FindBy(xpath = "//div[@class='alert alert-danger']//li")
+    private WebElement registrationError;
+
+    public void goToRegisterPage(String email) {
+        emailInput.sendKeys(email);
+        createAccountBtn.click();
     }
 
-    public void iFillTheFirstNameInput() {
-        firstNameInput.sendKeys("John");
+    public String getEmailErrorMessage() {
+        return emailErrorMessage.getText();
     }
 
-    public void iFillTheLastNameInput() {
-        lastNameInput.sendKeys("Does");
+    public void selectGender(String genre) {
+        if (genre.equalsIgnoreCase("Mr")) {
+            genderMr.click();
+        } else {
+            genderMrs.click();
+        }
     }
 
-    public void iFillTheAddressInput() {
-        addressInput.sendKeys("i dk");
+    public void fillName(String prenom, String nom) {
+        firstNameInput.sendKeys(prenom);
+        lastNameInput.sendKeys(nom);
     }
 
-    public void iFillTheCityInput() {
-        cityInput.sendKeys("Melun");
+    public void fillPassword(String password) {
+        passwordInput.sendKeys(password);
     }
 
-    public void iFillTheStateInput() {
-        stateInput.sendKeys("france");
+    public void selectDate(String date_naissance) {
+        if (date_naissance != null && !date_naissance.isEmpty()) {
+            String[] parts = date_naissance.split("/");
+            new Select(daysSelect).selectByValue(parts[0]);
+            new Select(monthsSelect).selectByValue(parts[1]);
+            new Select(yearsSelect).selectByValue(parts[2]);
+        }
     }
 
-    public void iFillThePhoneInput() {
-        phoneInput.sendKeys("0615169636");
+    public void checkNewsletter(String choix) {
+        if (choix.equalsIgnoreCase("oui")) {
+            if (!newsletterCheckbox.isSelected()) {
+                newsletterCheckbox.click();
+            }
+        }
     }
 
-    public void iFillTheSSNInput() {
-        ssnInput.sendKeys("qsdqshdqshdiqsh");
+    public void clickRegister() {
+        registerBtn.click();
     }
 
-    public void iFillTheUsernameInput() {
-        usernameInput.sendKeys("JohnDoe");
+    public String getFinalMessage() {
+        try {
+            return successMessage.getText(); // "Your account has been created."
+        } catch (NoSuchElementException e) {
+            return registrationError.getText(); // Erreur de mot de passe ou autre
+        }
     }
 
-    public void iFillThePasswordInput() {
-        passwordInput.sendKeys("test123*");
-    }
-
-    public void iFillTheConfirmInput() {
-        repeatedPasswordInput.sendKeys("test123*");
-    }
-
-    public void iFillTheZipCodecInput() {
-        zipCodeInput.sendKeys("77000");
-    }
-
-    public String getSuccessMessage(){
-        return succesMessage.getText();
-    }
-
-    public void iClickOnRegister(){
-        registerButton.click();
-    }
 }
--- src/test/java/steps/RegisterSteps.java
+++ src/test/java/steps/RegisterSteps.java
@@ -1,96 +1,21 @@
 package steps;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.junit.Assert;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.support.PageFactory;
 import pages.RegisterPage;
+import utils.DriverHelper;
 
 public class RegisterSteps {
-    private final RegisterPage registerPage;
 
-    public RegisterSteps() {
-        this.registerPage = new RegisterPage();
+
+    RegisterPage registerPage = PageFactory.initElements(DriverHelper.driver, RegisterPage.class);
+    String generatedEmail;
+
+
+    @Given("L'utilisateur est sur la page {string}")
+    public void utilisateurEstSurLaPage(String page) {
+        
     }
 
-    @Then("Im redirected to the Signup page")
-    public void imRedirectedToTheSignupPage() {
-        String formTitle = registerPage.imRedirectedToTheSignupPage();
-
-        Assert.assertEquals("Signing up is easy!", formTitle);
-    }
-
-    @And("I fill the first name input")
-    public void iFillTheFirstNameInput() {
-        registerPage.iFillTheFirstNameInput();
-    }
-
-    @And("I fill the last name input")
-    public void iFillTheLastNameInput() {
-        registerPage.iFillTheLastNameInput();
-    }
-
-    @And("I fill the Address input")
-    public void iFillTheAddressInput() {
-        registerPage.iFillTheAddressInput();
-    }
-
-    @And("I fill the City input")
-    public void iFillTheCityinput() {
-        registerPage.iFillTheCityInput();
-    }
-
-    @And("I fill the State input")
-    public void iFillTheStateInput() {
-        registerPage.iFillTheStateInput();
-    }
-
-    @And("I fill the Zip Code input")
-    public void iFillTheZipCodecInput() {
-       registerPage.iFillTheZipCodecInput();
-    }
-
-    @And("I fill the Phone input")
-    public void iFillThePhoneInput() {
-        registerPage.iFillThePhoneInput();
-
-    }
-
-    @And("I fill the SSN input")
-    public void iFillTheSSNInput() {
-        registerPage.iFillTheSSNInput();
-    }
-
-    @And("I fill the Username input")
-    public void iFillTheUsernameInput() {
-        registerPage.iFillTheUsernameInput();
-    }
-
-    @And("I fill the Password input")
-    public void iFillThePasswordInput() {
-        registerPage.iFillThePasswordInput();
-    }
-
-    @And("I fill the Confirm input")
-    public void iFillTheConfirmInput() {
-        registerPage.iFillTheConfirmInput();
-    }
-
-    @And("I clock on register")
-    public void iClockOnRegister() {
-        registerPage.iClickOnRegister();
-    }
-
-    @And("Im successfully registered")
-    public void imSuccessfullyRegistered() {
-        String succesMessage = registerPage.getSuccessMessage();
-        Assert.assertEquals("Your account was created successfully. You are now logged in.",succesMessage);
-    }
-
-
-    @Given("the user isn't already registered")
-    public void theUserIsnTAlreadyRegistered() {
-        return;
-    }
 }
