d6ada253d8721ffd9e4bec218b2a2029e6b2ffa7
Auralion <60516315+hkanoute@users.noreply.github.com>
2025-04-20 20:24:59 +0200

    Update DriverHelper.java

--- src/test/java/utils/DriverHelper.java
+++ src/test/java/utils/DriverHelper.java
@@ -34,9 +34,10 @@ public class DriverHelper {
         String remoteUrl = ConfigReader.getProperty("REMOTE_URL");
 
         if (isRemote) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName(browser.equalsIgnoreCase("Edge") ? "MicrosoftEdge" : browser.toLowerCase());
-            driver = new RemoteWebDriver(new URL(remoteUrl), capabilities);
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--user-data-dir=/tmp/profile-" + UUID.randomUUID());
         } else {
             switch (browser.toLowerCase()) {
                 case "firefox":
